cea61b33db2dcfcfc6331667ecf4d49cd8f4adec
ASHISK404 <116161048+ASHISK404@users.noreply.github.com>
2023-01-13 23:04:39 +0800

    Delete JS directory

--- JS/p4.js
+++
@@ -1,6 +0,0 @@
-let cai = document.querySelector('.cai')
-let op = document.querySelector('.op')
-cai.addEventListener('click', () => {
-    cai.classList.toggle('open')
-    op.classList.toggle('open')
-})
